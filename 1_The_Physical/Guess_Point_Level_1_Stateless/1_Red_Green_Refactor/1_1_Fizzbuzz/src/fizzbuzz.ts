0f10c8c4dc4194c0ed4be5fb36f34ae055ae4418
Otávio Araújo <otaviotech@gmail.com>
2023-04-05 17:42:04 +0100

    [new] fizzbuzz throws if input is not between 0 and 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,11 @@
 export function fizzbuzz(n: number) {
+  if (n < 1) {
+    throw new Error("n must be greater than 0");
+  }
+
+  if (n > 100) {
+    throw new Error("n must be less than 101");
+  }
+
   return "";
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,12 @@ describe("fizzbuzz", () => {
   it("should return a string", () => {
     expect(typeof fizzbuzz(1)).toBe("string");
   });
+
+  it("should throw if n is less than 1", () => {
+    expect(() => fizzbuzz(0)).toThrow(new Error("n must be greater than 0"));
+  });
+
+  it("should throw if n is greater than 100", () => {
+    expect(() => fizzbuzz(101)).toThrow(new Error("n must be less than 101"));
+  });
 });
