116511e70826de59cf31004af3f09ab2ae7fb7b6
Otávio Araújo <otaviotech@gmail.com>
2023-04-05 17:37:35 +0100

    [new] fizzbuzz returns a string

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export function fizzbuzz(n: number) {
+  return "";
+}
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("should return a string", () => {
+    expect(typeof fizzbuzz(1)).toBe("string");
+  });
 });
